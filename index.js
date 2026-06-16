5f1c3fd4cda2c95103dafc32a8a26615480ce568
Aneeb Iqbal <63883440+aneebiqbal@users.noreply.github.com>
2026-06-17 00:46:09 +0500

    fix cors

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5175"],
+    origin: 'https://frontend-inky-eight-34.vercel.app',
     credentials: true,
   })
 );
