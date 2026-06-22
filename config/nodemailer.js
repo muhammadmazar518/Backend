ef658d243cb4ca38fd398e2d7a11f6d304e8f28c
Aneeb Iqbal <63883440+aneebiqbal@users.noreply.github.com>
2026-06-22 15:09:49 +0500

    fix nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,11 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
 });
 
 module.exports = transporter;
